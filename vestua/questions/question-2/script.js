62f1c99ac6c94b6da620a057e4b22857aee87423
vanemcb <vanem2201@gmail.com>
2022-02-16 15:19:26 -0500

    Archivo CSV generado

--- vestua/questions/question-2/script.js
+++ vestua/questions/question-2/script.js
@@ -19,7 +19,8 @@ const arrayData = arrayLines.map(row => row.split(","));
 
 //Creating Product ID array without duplicates
 let arrayProductId = arrayData.map(row2 => row2[1])
-arrayProductId = [...new Set(arrayProductId)]
+arrayProductId = [...new Set(arrayProductId)].sort()
+arrayProductId.pop()
 
 //Creating User array without duplicates
 let arrayUser = arrayData.map(row2 => row2[0])
@@ -37,16 +38,47 @@ for (let user of arrayUser) {
     if (item[0] == user){
       if (item[3] == 'click'){
         generalDict[user].clicks.push(item[1])
-        generalDict[user].clicks = [...new Set(generalDict[user].clicks)]
       }
       if (item[3] == 'impression'){
         generalDict[user].visits.push(item[1])
-        generalDict[user].visits = [...new Set(generalDict[user].visits)]
       }
     }
   }
 }
 
+//Deleting products ID duplicates
+for (let user2 in generalDict){
+  generalDict[user2].clicks = [...new Set(generalDict[user2].clicks)].sort()
+  generalDict[user2].visits = [...new Set(generalDict[user2].visits)].sort()
+}
 
+//Creating final array to convert CSV file
+let finalArray = []
+for (let product of arrayProductId) {
+  rowArray = [product, 0, 0, 0]
 
-console.log(generalDict)
+  for (let user3 in generalDict){
+    for (click of generalDict[user3].clicks){
+      if (click == product){
+        rowArray[1] += 1
+      }
+    }
+    for (visit of generalDict[user3].visits){
+      if (visit == product){
+        rowArray[2] += 1
+      }
+    }
+  }
+  rowArray[3] = (rowArray[1] / rowArray[2]).toFixed(4)
+  finalArray.push(rowArray)
+}
+
+//Convert array to string
+let csvContent = finalArray.map(item => item.join(",")).join("\n");
+const headers = 'productId,clicks,impressions,ctr\n'
+
+//Creating CVS file
+fs.appendFile('output.csv', headers + csvContent, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
