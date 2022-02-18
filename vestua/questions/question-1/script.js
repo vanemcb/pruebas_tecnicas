eab0900f7b945902eb278031fd0ef85adcc5cd07
vanemcb <vanem2201@gmail.com>
2022-02-18 15:37:46 -0500

    Ejercicio 1 funcionando con valores menores de 40

--- vestua/questions/question-1/script.js
+++ vestua/questions/question-1/script.js
@@ -6,3 +6,28 @@
 
 const args = process.argv.slice(-1);
 console.log(`Running question #1 with args ${args}`)
+
+let steps = [1, 2]
+let N = parseInt(args)
+
+function stairs(steps, N) {
+
+  if (N == 0) {
+    return 1
+  }
+
+  if (N < 0) {
+    return 0
+  }
+
+  let combinations = 0
+
+  for (let step of steps) {
+    combinations += stairs(steps, N - step)
+  }
+
+  return combinations
+}
+
+comb = stairs(steps, N)
+console.log(comb)
